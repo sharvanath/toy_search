b3a0b5cd21cad05933a77345f12548d4ba389c23
Sharvanath Pathak <sharvanath@gmail.com>
2020-10-24 01:23:14 -0700

    implement

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/sharvanath/toy_search/core"
+)
+
+var searchIndex core.SearchIndex
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<title>ToySearch</title>")
+	queryStr := r.URL.Query().Get("q")
+
+	fmt.Fprintf(w, "<h2>Results for %s: </h2>", queryStr)
+	for _, r := range searchIndex.Search(queryStr) {
+		r.WriteToHtml(&w)
+		fmt.Fprintf(w, "<br>")
+	}
+}
 
 func main() {
-	fmt.Printf("hello")
+	searchIndex.IndexDir("./datasets/space")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
